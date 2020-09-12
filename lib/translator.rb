b4ee4ea95dc7826dcbb933b37d8b7a9d10e2f59a
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 17:56:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,26 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  new_emotes = {}
+
+  emoticons.each do | key, value |
+    new_emotes[key] = {}
+    new_emotes[key][:english] = value[0]
+    new_emotes[key][:japanese] = value[1]
+  end
+  new_emotes
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  load_library(path).each do | key, value |
+    if value[:english] == emoticon
+      return key
+    end
+  end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
