d779c79bc627e5ca17831504c15e101ce5a641f5
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-12 17:59:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,9 @@ def get_japanese_emoticon(path, emoticon)
 end
 
 def get_english_meaning(path, emoticon)
-
+  load_library(path).each do | key, value |
+    if value[:japanese] == emoticon
+      return value[:english]
+    end
+  end
 end
